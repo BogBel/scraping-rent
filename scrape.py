8dab792e76e9d01deef20259fcf3242a7ef550aa
Your Name <bbeloshitskyy@gmail.com>
2017-02-16 17:49:58 +0200

    write

--- scrape.py
+++ scrape.py
@@ -1,45 +1,71 @@
 from multiprocessing import Manager
 import json
 import argparse
+from copy import deepcopy
 
 from modules.oktv.oktv_main import run_module as ok_tv_module
 from modules.dobovo.dobovo_main import run_module as dobovo_module
 
 
 def get_args():
-    arg_parser = argparse.ArgumentParser(description='Scrape', add_help=False)
-    arg_parser.add_argument('--help', '-h',
-                            action='help',
-                            default=argparse.SUPPRESS,
-                            help='Search for sources calendars and make output like: '
-                                 '\tdate: True(if empty) else False')
-    arg_parser.add_argument('--write', '-w', action='store_true', help='Save output to json file', default=True)
+    arg_parser = argparse.ArgumentParser(description='Scrape Rent', add_help=True)
+    arg_parser.add_argument('--write', '-w', action='store', help='filename result will be stored in', default=False)
     arg_parser.add_argument('--show', '-s', action='store_true', help='Show output to stdout', default=True)
     return arg_parser
 
 
 def main():
+    """
+    Scrape sources in modules, collect rent calendar  and display/store it to json
+    :return:
+    {
+        "ok_tv": {
+           "https://oktv.ua/id3093608":{
+              "18.02.2017":True,
+              "28.03.2017":False,
+              "08.03.2017":True
+           },
+           "https://oktv.ua/id3093617":{
+              "18.02.2017":True,
+              "28.03.2017":True,
+              "08.03.2017":True,
+
+           },
+        },
+        "dobovo": {
+           "http://www.dobovo.com/kiev-apartments/elite-apartment-45244.html":{
+              "18.02.2017":True,
+              "28.03.2017":False,
+              "08.03.2017":True,
+
+           },
+           "http://www.dobovo.com/kiev-apartments/city-center-23668.html":{
+              "18.02.2017":True,
+              "28.03.2017":True,
+              "08.03.2017":False,
+
+           }
+        }
+    }
+    """
     with Manager() as manager:
         multiprocessing_dict = manager.dict()
         child_processes = list()
         child_processes.append(dobovo_module(multiprocessing_dict))
         child_processes.append(ok_tv_module(multiprocessing_dict))
-        arg_parser = get_args().parse_args()
-
-        # try:
-        #     for process in child_processes:
-        #         process.start()
-        # finally:
-        #     for process in child_processes:
-        #         process.join()
-        # args = get_args()
-        # if args.help:
-        #     print('suka')
-        #     raise KeyError
-        # print(multiprocessing_dict)
-        # with open('data.txt', 'w') as outfile:
-        #     json.dump(data, outfile)
-        # json.dumps(multiprocessing_dict)
+        args = vars(get_args().parse_args())
+        print(args)
+        try:
+            for process in child_processes:
+                process.start()
+        finally:
+            for process in child_processes:
+                process.join()
+        print(multiprocessing_dict)
+        filename = args.get('write')
+        if filename:
+            with open('{}.json'.format(filename), 'w') as outfile:
+                json.dumps(deepcopy(multiprocessing_dict), outfile)
 
 if __name__ == '__main__':
     main()
